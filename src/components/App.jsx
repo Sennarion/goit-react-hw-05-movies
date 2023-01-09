3931d4cbb18c19899607bc995e68f1359699e836
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-01-09 21:09:21 +0200

    change * path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,13 @@ export default function App() {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<Home />} />
       </Route>
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
